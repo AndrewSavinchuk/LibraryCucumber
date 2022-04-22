6b6d0eb63c6af2fb08f35582d2632175c4c2963a
CrazyDevo <adam@cybertekschool.com>
2022-04-22 16:39:51 -0400

    US6  is done

--- src/test/java/com/library/step_definitions/BooksStepDefs.java
+++ src/test/java/com/library/step_definitions/BooksStepDefs.java
@@ -11,6 +11,8 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 
+import java.util.List;
+
 public class BooksStepDefs {
     LoginPage loginPage;
     //LoginPage loginPage =new LoginPage();
@@ -105,18 +107,24 @@ public class BooksStepDefs {
         Assert.assertEquals(ecpectedYear, actualYear);
 
     }
-
+    List<String> actualBookCategories;
     @When("I take all book categories in webpage")
     public void i_take_all_book_categories_in_webpage() {
-
+        actualBookCategories = BrowserUtil.getAllSelectOptions(new BookPage().mainCategoryElement);
+        actualBookCategories.remove(0);
     }
     @When("I execute query to get book categories")
     public void i_execute_query_to_get_book_categories() {
+String query="SELECT name\n" +
+        "FROM book_categories";
+DB_Util.runQuery(query);
+
 
     }
     @Then("verify book categories must match book_categories table from db")
     public void verify_book_categories_must_match_book_categories_table_from_db() {
-
+        List<String> expectedBookCategories = DB_Util.getColumnDataAsList(1);
+        Assert.assertEquals(expectedBookCategories, actualBookCategories);
     }
 
 
